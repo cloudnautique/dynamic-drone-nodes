66cb9e45ff39ef156e27b1b9fd778b6dddb7d753
Bill Maxwell <bill.t.maxwell@me.com>
2016-02-02 22:57:04 -0700

    ready for v0.1.0

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/cloudnautique/dynamic-drone-nodes/version"
 	"github.com/codegangsta/cli"
 	"github.com/drone/drone-go/drone"
+)
+
+const (
+	metadataURL = "http://rancher-metadata/2015-12-19"
 )
 
 func main() {
@@ -13,6 +19,7 @@ func main() {
 	app.Name = "dynamic-drone-nodes"
 	app.Usage = "Dynamically add and remove Drone CI nodes"
 	app.Action = appInit
+	app.Version = version.VERSION
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:   "drone-token",
@@ -23,6 +30,11 @@ func main() {
 			Name:   "drone-url",
 			Usage:  "URL for the Drone CI server",
 			EnvVar: "DRONE_URL",
+		},
+		cli.IntFlag{
+			Name:  "poll-interval",
+			Usage: "Interval in (s) to poll dynamic pool",
+			Value: 300,
 		},
 	}
 
@@ -40,19 +52,33 @@ func appInit(c *cli.Context) {
 		logrus.Fatal("Drone CI server URL is missing")
 	}
 
+	if len(c.Args()) < 0 {
+		logrus.Fatal("No path to watch found")
+	}
+	path := c.Args()[0]
+
 	droneClient := &Drone{drone.NewClientToken(droneURL, droneToken)}
 
-	PoolClient, err := NewPoolClient("http://rancher-metadata/2015-12-19")
+	poolClient, err := NewPoolClient(metadataURL)
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
-	poolNodes, err := PoolClient.ListNodes("docker")
-	if err != nil {
-		logrus.Fatal(err)
-	}
+	run(droneClient, poolClient, path, c.Int("poll-interval"))
+}
 
-	if err = droneClient.ReconcileNodeDifferences(convertNodeToMap(poolNodes)); err != nil {
-		logrus.Fatal(err)
+func run(droneClient *Drone, poolClient DynamicNodePool, path string, interval int) {
+	logrus.Infof("Path: %s", path)
+	for {
+		poolNodes, err := poolClient.ListNodes(path)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+
+		if err = droneClient.ReconcileNodeDifferences(convertNodeToMap(poolNodes)); err != nil {
+			logrus.Fatal(err)
+		}
+
+		time.Sleep(time.Duration(interval) * time.Second)
 	}
 }
--- rancher.go
+++ rancher.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"errors"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
 	"github.com/drone/drone-go/drone"
 	"github.com/rancher/go-rancher-metadata/metadata"
 )
 
 type RancherClient struct {
 	client *metadata.Client
+}
+
+type RancherStackServicePair struct {
+	Stack   string
+	Service string
 }
 
 type DynamicNodePool interface {
@@ -28,8 +37,16 @@ func NewRancherClient(endpoint string) (*metadata.Client, error) {
 
 func (c *RancherClient) ListNodes(nodesPath string) ([]*drone.Node, error) {
 	nodes := []*drone.Node{}
+	stackServicePair, err := segmentPathToStackServicePair(nodesPath)
+	if err != nil {
+		logrus.Fatal(err)
+	}
 
-	rancherNodes, _ := c.client.GetServiceContainers("docker", "test")
+	rancherNodes, err := c.client.GetServiceContainers(stackServicePair.Service, stackServicePair.Stack)
+	if err != nil {
+		logrus.Fatalf("Could not get containers from. %s", err)
+	}
+
 	for _, rNode := range rancherNodes {
 		n := &drone.Node{
 			Addr: "tcp://" + rNode.PrimaryIp + ":2375",
@@ -39,4 +56,27 @@ func (c *RancherClient) ListNodes(nodesPath string) ([]*drone.Node, error) {
 	}
 
 	return nodes, nil
+}
+
+// Path should be /stacks/<stackname>/services/<servicename>
+func segmentPathToStackServicePair(path string) (*RancherStackServicePair, error) {
+	rSSP := &RancherStackServicePair{}
+	err := errors.New("Unable to parse path: " + path + "\n" +
+		"expected format is: /stacks/<stackname>/services/<servicename>")
+
+	path = strings.TrimPrefix(path, "/")
+	segments := strings.Split(path, "/")
+	if len(segments) != 4 {
+		logrus.Error(len(segments))
+		logrus.Error(segments)
+		return rSSP, err
+	}
+
+	if segments[0] == "stacks" && segments[2] == "services" {
+		rSSP.Stack = segments[1]
+		rSSP.Service = segments[3]
+		err = nil
+	}
+
+	return rSSP, err
 }
